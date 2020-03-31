bb272ff5a3a3206bb0e2f8737e6dc46e1ae0c63c
OriginalPanda <lamsjr37@gmail.com>
2020-04-01 00:12:41 +0100

    HealthApp MK2(fixed) CSS + complete application

--- src/sample/Progress.java
+++ src/sample/Progress.java
@@ -5,20 +5,25 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ProgressBar;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+/**
+ * Controller class containing various methods to update views of the Progress page
+ */
 
 public class Progress {
-    //@FXML
-    //private Button graphButton;
 
     @FXML
     private ProgressBar weightProgressBar, distanceProgressBar;
 
+    /**
+     * Method to display the user's weight progression in the form of a graph
+     */
     public void displayWeightGraph() {
         Stage stage = new Stage();
         stage.setTitle("Kenko");
@@ -70,6 +75,9 @@ public class Progress {
     }
 
 
+    /**
+     * Method to display the distance graph (tracking the user's distance progress)
+     */
     public void displayGraphDistance() {
         Stage stage = new Stage();
         stage.setTitle("Kenko");
@@ -121,6 +129,10 @@ public class Progress {
         stage.show();
     }
 
+    /**
+     * Method to retrieve the current user's username
+     * @return username
+     */
     public String currentUser() {
         BufferedReader bufferedReader = null;
         String fileName = "currentUser.csv";
@@ -136,15 +148,20 @@ public class Progress {
         return null;
     }
 
+    /**
+     * Method to update both of the progress bars with how close to user's goal weight and distance they are.
+     */
     public void updateProgressBars() {
         distanceProgressBar.setProgress(distanceProgressValue());
         weightProgressBar.setProgress(weightProgressValue());
     }
 
+    /**
+     * Method to retrieve the user's current weight
+     * @return currentWeight
+     */
     public double getCurrWeight() {
         double currWeight = 0;
-        //populating the series with data
-        // Reading from CSV and populating the series with the data.
         BufferedReader bufferedReader = null;
         String fileName = "weights.csv";
         String line = "";
@@ -162,15 +179,18 @@ public class Progress {
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
+        System.out.println("curr" + currWeight);
         return currWeight;
     }
 
+    /**
+     * Method to retrieve the user's goal weight
+     * @return goalWeight
+     */
     public double getGoalWeight() {
         double goalWeight = 0;
-        //populating the series with data
-        // Reading from CSV and populating the series with the data.
         BufferedReader bufferedReader = null;
-        String fileName = "weights.csv";
+        String fileName = "weightGoal.csv";
         String line = "";
         String csvSplitBy = ",";
         try {
@@ -186,17 +206,26 @@ public class Progress {
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
+        System.out.println(goalWeight);
         return goalWeight;
     }
 
-
+    /**
+     * Method to calculate how close the user is to hitting their goal weight using goal & current weight
+     * @return weightProgressValue (as a decimal)
+     */
     public double weightProgressValue() {
         double weightValue = 0;
         weightValue = (getGoalWeight() / getCurrWeight());
+        System.out.println(weightValue);
         return weightValue;
     }
 
 
+    /**
+     * Method to retrieve the user's current distance
+     * @return currentDistance
+     */
     public double getCurrDistance() {
         double currDistance = 0;
         //populating the series with data
@@ -221,12 +250,16 @@ public class Progress {
         return currDistance;
     }
 
+    /**
+     * Method to retrieve the user's goal distance
+     * @return goalDistance
+     */
     public double getGoalDistance() {
         double goalDistance = 0;
         //populating the series with data
         // Reading from CSV and populating the series with the data.
         BufferedReader bufferedReader = null;
-        String fileName = "distances.csv";
+        String fileName = "distanceGoal.csv";
         String line = "";
         String csvSplitBy = ",";
         try {
@@ -245,8 +278,62 @@ public class Progress {
         return goalDistance;
     }
 
+    /**
+     * Method to calculate how close the user is to hitting their goal distance using goal & current weight
+     * @return distanceProgressValue (as a decimal)
+     */
     public double distanceProgressValue() {
         double weightValue = (getCurrDistance() / getGoalDistance());
         return weightValue;
+    }
+
+    /**
+     * Method to retrieve the user's height
+     * @return height
+     */
+    public double getHeight() {
+        double height = 0;
+        //populating the series with data
+        // Reading from CSV and populating the series with the data.
+        BufferedReader bufferedReader = null;
+        String fileName = "accounts.csv";
+        String line = "";
+        String csvSplitBy = ",";
+        try {
+            bufferedReader = new BufferedReader(new FileReader(fileName));
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] split = line.split(csvSplitBy);
+                String username = split[0];
+                if (username.equals(currentUser()))
+                {
+                    height = Integer.parseInt(split[(6)]);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        System.out.println(height);
+        return height;
+    }
+
+
+    /**
+     * Method to calculate the BMI of the logged in user from their height and weight
+     * @return BMI value
+     */
+    public double calculateBMI(){
+        return (getCurrWeight()/(getHeight()*getHeight()));
+    }
+
+    public void displayBMI()
+    {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Kenko");
+        alert.setHeaderText("Your BMI is: " + calculateBMI());
+        alert.setContentText("below 18.5 – you're in the underweight range.\n" +
+                "between 18.5 and 24.9 – you're in the healthy weight range.\n" +
+                "between 25 and 29.9 – you're in the overweight range.\n" +
+                "between 30 and 39.9 – you're in the obese range.");
+        alert.showAndWait();
     }
 }
